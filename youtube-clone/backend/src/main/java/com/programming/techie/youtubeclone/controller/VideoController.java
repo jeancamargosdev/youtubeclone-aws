1ff4302604bf6b8e45b5663c1ff924995c4639ca
jmcamargos <jeanmatheus@hotmail.com>
2023-01-27 10:07:30 -0300

    Commiting all backend

---
+++ youtube-clone/backend/src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
@@ -0,0 +1,34 @@
+package com.programming.techie.youtubeclone.controller;
+
+import com.programming.techie.youtubeclone.service.VideoService;
+import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+//@SpringBootApplication
+@RestController
+@RequestMapping("/api/videos")
+@RequiredArgsConstructor
+public class VideoController {
+    private final VideoService videoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void uploadVideo(@RequestParam("file") MultipartFile file){
+        videoService.uploadVideo(file);
+    }
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void test(){
+
+    }
+
+}
