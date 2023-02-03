b053ab50db61a2be58e5b45373548c33188a42e1
jmcamargos <jeanmatheus@hotmail.com>
2023-02-03 10:00:44 -0300

    part 5 completed

--- youtube-clone/backend/src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
+++ youtube-clone/backend/src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
@@ -1,11 +1,15 @@
 package com.programming.techie.youtubeclone.controller;
 
+import com.programming.techie.youtubeclone.dto.UploadVideoResponse;
+import com.programming.techie.youtubeclone.dto.VideoDto;
 import com.programming.techie.youtubeclone.service.VideoService;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,16 +23,33 @@ import org.springframework.web.multipart.MultipartFile;
 public class VideoController {
     private final VideoService videoService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void uploadVideo(@RequestParam("file") MultipartFile file){
-        videoService.uploadVideo(file);
-    }
-
     @GetMapping("/test")
     @ResponseStatus(HttpStatus.CREATED)
     public void test(){
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UploadVideoResponse uploadVideo(@RequestParam("file") MultipartFile file){
+        return videoService.uploadVideo(file);
 
     }
+
+    @PostMapping("/thumbnail")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String  uploadThumbnail(@RequestParam("file") MultipartFile file, @RequestParam("videoId") String videoId){
+
+        return videoService.uploadThumbnail(file, videoId);
+    }
+
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public VideoDto editVideoMetadata(@RequestBody VideoDto videoDto){
+        return videoService.editVideo(videoDto);
+
+    }
+
+
 
 }
--- youtube-clone/backend/src/main/java/com/programming/techie/youtubeclone/service/VideoService.java
+++ youtube-clone/backend/src/main/java/com/programming/techie/youtubeclone/service/VideoService.java
@@ -1,5 +1,7 @@
 package com.programming.techie.youtubeclone.service;
 
+import com.programming.techie.youtubeclone.dto.UploadVideoResponse;
+import com.programming.techie.youtubeclone.dto.VideoDto;
 import com.programming.techie.youtubeclone.model.Video;
 import com.programming.techie.youtubeclone.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,44 @@ public class VideoService {
 
     private final S3Service s3Service;
     private final VideoRepository videoRepository;
-    public void uploadVideo(MultipartFile multipartFile){        //Uppload file do AWS S3 and Save Video Data to Database
+    public UploadVideoResponse uploadVideo(MultipartFile multipartFile){        //Uppload file do AWS S3 and Save Video Data to Database
         String videoUrl = s3Service.uploadFile(multipartFile);
         var video = new Video();
         video.setVideoUrl(videoUrl);
 
-        videoRepository.save(video);
+        var savedVideo = videoRepository.save(video);
+        return new UploadVideoResponse(savedVideo.getId(), savedVideo.getVideoUrl());
+    }
+
+    public VideoDto editVideo(VideoDto videoDto) {
+
+        var savedVideo = getVideoById((videoDto.getId()));
+
+        savedVideo.setTitle(videoDto.getTitle());
+        savedVideo.setDescription(videoDto.getDescription());
+        savedVideo.setTags(videoDto.getTags());
+        savedVideo.setThumbnailUrl(videoDto.getThumbnailUrl());
+        savedVideo.setVideoStatus(videoDto.getVideoStatus());
+
+        videoRepository.save(savedVideo);
+        return videoDto;
+    }
+
+    public String uploadThumbnail(MultipartFile file, String videoId) {
+        var savedVideo = getVideoById(videoId);
+
+       String thumbnailUrl =  s3Service.uploadFile(file);
+
+       savedVideo.setThumbnailUrl(thumbnailUrl);
+
+       videoRepository.save(savedVideo);
+
+       return thumbnailUrl;
+
+    }
+
+    Video getVideoById(String videoId){
+        return videoRepository.findById(videoId)
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find video by id - " + videoId));
     }
 }
